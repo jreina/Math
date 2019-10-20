7442631984e5e57dfedd66d78df1c6f3dbf05b2c
Austin McMillon <austin.mcmillon@gmail.com>
2019-10-19 20:17:08 -0500

    Underground Funcs

--- src/analysis/index.ts
+++ src/analysis/index.ts
@@ -1,1 +1,11 @@
-export const analysis = {};
+export const analysis = {
+    /**
+     * This is the way to find nodes for a Chebychev Polynomial, T_n(x) = cos(narcos(x)) for |x| <= 1. Useful for approximating values of cosine up to the amount of nodes you use.
+     */
+    chebychevPoly(n: number,
+        x: number) {
+            let tn1 = (2 * x * Math.cos(n * Math.acos(x))) - (Math.cos((n-1) * Math.acos(x)));
+            return tn1 
+        }
+};
+
