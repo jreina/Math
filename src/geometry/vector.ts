01285f08907a180718d50c4b8e6f5864ea58a812
Johnny Reina <johnny.r.reina@gmail.com>
2019-10-20 19:04:04 -0500

    Adds vector dot product

--- src/geometry/vector.ts
+++ src/geometry/vector.ts
@@ -1,3 +1,13 @@
+import { Vector2D } from '../types/Vector2D';
+import { Vector3D } from '../types/Vector3D';
+import { algebra } from '../algebra';
+
+function dotProduct(a: Vector3D, b: Vector3D): number;
+function dotProduct(a: Vector2D, b: Vector2D): number;
+function dotProduct(a: Vector2D | Vector3D, b: Vector2D | Vector3D): number {
+  return a.map((val, i) => val * b[i]).reduce(algebra.add);
+}
+
 export const vector = {
   /**
    * Calculate the length of a vector in R_2 or R_3
@@ -8,5 +18,9 @@ export const vector = {
   length(i: number, j: number, k?: number): number {
     k = k || 0;
     return Math.sqrt(i * i + j * j + k * k);
-  }
+  },
+  /**
+   * Calculate the dot product of two vectors in R_2 or R_3
+   */
+  dotProduct
 };
